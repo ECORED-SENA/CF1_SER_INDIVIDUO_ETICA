89b64bc01f284d97b264f23cb590a4907bdc0fe7
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2026-02-04 15:00:26 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -109,7 +109,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CodProg_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
